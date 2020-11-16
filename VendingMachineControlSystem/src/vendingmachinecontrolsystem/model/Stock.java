7cfd90b88f55e999d9ded7e6d8f7e83ef4e9d739
dannellim <dannel_lim@hotmail.com>
2020-11-16 21:02:43 +0800

    added models

---
+++ VendingMachineControlSystem/src/vendingmachinecontrolsystem/model/Stock.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendingmachinecontrolsystem.model;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author Dannel
+ */
+public abstract class Stock extends Observable {
+
+    private String name;
+    private double value;
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        setChanged();
+        notifyObservers();
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+        setChanged();
+        notifyObservers();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName()+ "{" + "name=" + name + ", value=" + value + ", quantity=" + quantity + '}';
+    }
+}
