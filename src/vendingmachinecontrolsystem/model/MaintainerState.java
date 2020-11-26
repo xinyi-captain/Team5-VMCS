d5f3b19d55a7a5165d8a46bca290ee47158a86c2
dongxinyi <dongxinyi@10.10.21.208>
2020-11-26 22:17:08 +0800

    add maintainer state

---
+++ src/vendingmachinecontrolsystem/model/MaintainerState.java
@@ -0,0 +1,35 @@
+package vendingmachinecontrolsystem.model;
+
+import java.util.Observable;
+
+public class MaintainerState extends Observable{
+	private boolean isLogIn;
+
+	private  static  MaintainerState maintainerState;
+	
+	private MaintainerState(){
+		isLogIn=true;
+	}
+	
+	public static MaintainerState getInstance() {
+		if (maintainerState == null) {
+			synchronized (DoorState.class) {
+				if (maintainerState == null) {
+					maintainerState = new MaintainerState();
+				}
+			}
+		}
+		return maintainerState;
+	}
+	
+	public boolean isLocked() {
+		return isLogIn;
+	}
+
+	public void setLogIn(boolean isLogIn) {
+		this.isLogIn = isLogIn;
+        setChanged();
+        notifyObservers();
+	}
+	
+}
