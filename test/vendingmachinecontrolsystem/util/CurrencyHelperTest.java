956c246e890947057c8b608e0f069eb225224403
dongxinyi <dongxinyi@10.10.21.208>
2020-11-28 13:59:40 +0800

    currencyHelper test

---
+++ test/vendingmachinecontrolsystem/util/CurrencyHelperTest.java
@@ -0,0 +1,14 @@
+package vendingmachinecontrolsystem.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CurrencyHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
