f5f56ee58823f6b0a5b5d0ed3f6f47db9ed44b21
dongxinyi <dongxinyi@10.10.21.208>
2020-11-28 13:54:52 +0800

    test commit

---
+++ test/vendingmachinecontrolsystem/controller/MaintainerControllerTest.java
@@ -0,0 +1,64 @@
+package vendingmachinecontrolsystem.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaintainerControllerTest {
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnlock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowPanel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClosePanel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDrinkStocks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCoinStocks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollectAllCash() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowTotalCashHeld() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+}
