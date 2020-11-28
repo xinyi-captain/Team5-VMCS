fc5c159f5e45ef6155cd1816a1c9007ad47bc0d9
dongxinyi <dongxinyi@10.10.21.208>
2020-11-28 14:31:11 +0800

    machineryController test

---
+++ test/vendingmachinecontrolsystem/controller/MachineryControllerTest.java
@@ -0,0 +1,14 @@
+package vendingmachinecontrolsystem.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MachineryControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
